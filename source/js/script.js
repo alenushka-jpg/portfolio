754dc0752c942ed92a1250c83940fa85d2f45166
alenushka <alionxlo@gmail.com>
2022-10-27 21:18:20 +0300

    Writes script for the drop-down

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,19 @@
+const header = document.querySelector('.page-header');
+const burger = document.querySelector('.page-header__burger');
+// const body = document.querySelector('body');
 
+// function bodyHiddenToggle() {
+//   if (body !== 'body') {
+//     body.classList.toggle('page__body--hidden');
+//   };
+// };
 
+function showHeader() {
+  header.classList.toggle('page-header--open');
+};
 
+function onHeaderClick() {
+  showHeader();
+};
 
-
-
-
-
-
+burger.addEventListener('click', onHeaderClick);
