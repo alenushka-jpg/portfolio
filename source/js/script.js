389461f351bfff560a951415d7ebb1c92f54bb3f
alenushka <alionxlo@gmail.com>
2022-10-28 11:28:14 +0300

    Writes a script to open certificates

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,8 @@
 const header = document.querySelector('.page-header');
 const burger = document.querySelector('.page-header__burger');
-// const body = document.querySelector('body');
-
-// function bodyHiddenToggle() {
-//   if (body !== 'body') {
-//     body.classList.toggle('page__body--hidden');
-//   };
-// };
+const certificates = document.querySelector('.certificates');
+const certificatesOpen = document.querySelector('.promo___certificates');
+const certificatesClose = document.querySelector('.certificates__button');
 
 function showHeader() {
   header.classList.toggle('page-header--open');
@@ -17,3 +13,23 @@ function onHeaderClick() {
 };
 
 burger.addEventListener('click', onHeaderClick);
+
+
+function showCertificates() {
+  certificates.classList.add('certificates--open');
+};
+
+function onCertificatesClick() {
+  showCertificates();
+};
+
+function hiddenCertificates() {
+  certificates.classList.remove('certificates--open');
+};
+
+function onCloseCertificatesClick() {
+  hiddenCertificates();
+};
+
+certificatesOpen.addEventListener('click', onCertificatesClick);
+certificatesClose.addEventListener('click', hiddenCertificates);
